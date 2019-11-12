18eb34a387148c27aa6aced4db8c59a500354f6c
WFRCAnalytics <57676096+WFRCAnalytics@users.noreply.github.com>
2019-11-12 13:36:58 -0700

    Update cube_skimMTXtonormalizedIJdbf.s

--- cube_skimMTXtonormalizedIJdbf.s
+++ cube_skimMTXtonormalizedIJdbf.s
@@ -2,8 +2,8 @@ READ FILE = '0GeneralParameters.block';'..\..\0GeneralParameters.block'
 READ FILE = '1ControlCenter.block';'..\..\1ControlCenter.block'
 
 RUN PGM=MATRIX
-FILEI MATI[1]="E:\Model\Model16\RTP2015\Round22\V8.1 Master - SE40Hwy40Trn40\4_ModeChoice\Mo\1a_Skims\skm_auto_Pk.mtx"
-FILEI MATI[2]="E:\Model\Model16\RTP2015\Round22\V8.1 Master - SE40Hwy40Trn40\Temp\4_TmpMC\Best_Walk_Skims.mtx"
+FILEI MATI[1]="C:\yourpath\skm_auto_Pk.mtx"
+FILEI MATI[2]="C:\yourpath\Best_Walk_Skims.mtx"
 FILEO RECO[1]="autoskim.dbf" FIELDS=I,J,TIMEAUTO, TIMETRANSIT 
 
 mw[1]=mi.1.1 + mi.1.5
